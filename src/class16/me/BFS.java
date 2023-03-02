8e44d585214477f8206642810e35fa2a37de01b8
anna <xionglei1614@163.com>
2023-03-02 21:15:57 +0800

    add me code: 图的遍历

--- src/class16/me/BFS.java
+++ src/class16/me/BFS.java
@@ -1,14 +1,49 @@
 package class16.me;
 
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
 
 public class BFS {
 
-    // 宽度优先遍历，随便选一个节点
-    public <T> void BFS(Node<T> node) {
-        if (node == null) return;
-        Queue<Node<T>> queue = new LinkedList<>();
+    public static void main(String[] args) {
+        // 1 2 5 3
+        //  5 4
+        // 1 —— 2
+        //  \  /
+        //   5
+        //   |
+        //   3 - 4
+        int[][] path = {
+                {2, 1, 2},
+                {2, 2, 1},
+                {4, 1, 5},
+                {4, 5, 1},
+                {6, 2, 5},
+                {6, 5, 2},
+                {1, 3, 5},
+                {1, 5, 3},
+                {7, 4, 3},
+                {7, 3, 4}
+        };
+        Graph<Integer> graph = GraphBuilder.build(path);
+        bfs(graph.nodes.get(1));
+    }
 
+    // 宽度优先遍历，随便选一个节点
+    public static  <T> void bfs(Node<T> start) {
+        if (start == null) return;
+        Queue<Node<T>> queue = new LinkedList<>();
+        Set<Node<T>> set = new HashSet<>();
+        queue.offer(start);
+        set.add(start);
+        while (!queue.isEmpty()) {
+            Node<T> cur = queue.poll();
+            System.out.println(cur.val);
+            for (Node<T> node : cur.nexts) {
+                if (!set.contains(node)) {
+                    queue.offer(node);
+                    set.add(node);
+                }
+            }
+        }
     }
 }
