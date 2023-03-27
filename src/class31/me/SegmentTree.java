17469f76d27ff2ca27ad581026d35eba286afa44
xionglei <xionglei1614@163.com>
2023-03-27 18:09:26 +0800

    add me code: 熟悉线段树coding

---
+++ src/class31/me/SegmentTree.java
@@ -0,0 +1,179 @@
+package class31.me;
+
+import class31.Code01_SegmentTree;
+
+public class SegmentTree extends Code01_SegmentTree {
+
+    int MAX;
+    int[] arr;
+    int[] sum;
+    int[] lazy;
+    int[] change;
+    boolean[] update;
+
+    public SegmentTree(int[] origin) {
+        MAX = origin.length + 1;
+        arr = new int[MAX];
+        System.arraycopy(origin, 0, arr, 1, origin.length);
+        sum = new int[MAX << 2];
+        lazy = new int[MAX << 2];
+        change = new int[MAX << 2];
+        update = new boolean[MAX << 2];
+    }
+
+    public void pushup(int rt) {
+        sum[rt] = sum[rt << 1] + sum[rt << 1 | 1];
+    }
+
+    public void pushdown(int rt, int ln, int rn) {
+        if (update[rt]) {
+            update[rt << 1] = true;
+            update[rt << 1 | 1] = true;
+            change[rt << 1] = change[rt];
+            change[rt << 1 | 1] = change[rt];
+            lazy[rt << 1] = 0;
+            lazy[rt << 1 | 1] = 0;
+            sum[rt << 1] = ln * change[rt];
+            sum[rt << 1 | 1] = rn * change[rt];
+            update[rt] = false;
+        }
+        if (lazy[rt] != 0) {
+            lazy[rt << 1] += lazy[rt];
+            lazy[rt << 1 | 1] += lazy[rt];
+            sum[rt << 1] += ln * lazy[rt];
+            sum[rt << 1 | 1] += rn * lazy[rt];
+            lazy[rt] = 0;
+        }
+    }
+
+    // fill sum
+    public void build(int l, int r, int rt) {
+        if (l == r) {
+            sum[r] = arr[r];
+        }
+        int mid = l + ((r - l) >> 1);
+        build(l, mid, rt << 1);
+        build(mid + 1, r, rt << 1 | 1);
+        pushup(rt);
+    }
+
+    public void add(int L, int R, int C, int l, int r, int rt) {
+        if (L <= l && r <= R) {
+            lazy[rt] += C;
+            sum[rt] += (r - l + 1) * C;
+            return;
+        }
+        int mid = l + ((r - l) >> 1);
+        pushdown(rt, mid - l + 1, r - mid);
+        if (L <= mid) {
+            add(L, R, C, l, mid, rt << 1);
+        }
+        if (R > mid) {
+            add(L, R, C, mid + 1, r, rt << 1 | 1);
+        }
+        pushup(rt);
+    }
+
+    public void update(int L, int R, int C, int l, int r, int rt) {
+        if (L <= l && r <= R) {
+            update[rt] = true;
+            change[rt] = C;
+            sum[rt] = (r - l + 1) * C;
+            lazy[rt] = 0;
+            return;
+        }
+        int mid = l + ((r - l) >> 1);
+        pushdown(rt, mid - l + 1, r - mid);
+        if (L <= mid) {
+            update(L, R, C, l, mid, rt << 1);
+        }
+        if (R > mid) {
+            update(L, R, C, mid + 1, r, rt << 1 | 1);
+        }
+        pushup(rt);
+    }
+
+    public long query(int L, int R, int l, int r, int rt) {
+        if (L <= l && r <= R) {
+            return sum[rt];
+        }
+        int mid = l + ((r - l) >> 1);
+        pushdown(rt, mid - l + 1, r - mid);
+        int ans = 0;
+        if (L <= mid) {
+            ans += query(L, R, l, mid, rt << 1);
+        }
+        if (R > mid) {
+            ans += query(L, R, mid + 1, r, rt << 1 | 1);
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] origin = {2, 1, 1, 2, 3, 4, 5};
+        SegmentTree seg = new SegmentTree(origin);
+        int S = 1; // 整个区间的开始位置，规定从1开始，不从0开始 -> 固定
+        int N = origin.length; // 整个区间的结束位置，规定能到N，不是N-1 -> 固定
+        int root = 1; // 整棵树的头节点位置，规定是1，不是0 -> 固定
+        int L = 2; // 操作区间的开始位置 -> 可变
+        int R = 5; // 操作区间的结束位置 -> 可变
+        int C = 4; // 要加的数字或者要更新的数字 -> 可变
+        // 区间生成，必须在[S,N]整个范围上build
+        seg.build(S, N, root);
+        // 区间修改，可以改变L、R和C的值，其他值不可改变
+        seg.add(L, R, C, S, N, root);
+        // 区间更新，可以改变L、R和C的值，其他值不可改变
+        seg.update(L, R, C, S, N, root);
+        // 区间查询，可以改变L和R的值，其他值不可改变
+        long sum = seg.query(L, R, S, N, root);
+        System.out.println(sum);
+
+        System.out.println("对数器测试开始...");
+        System.out.println("测试结果 : " + (test() ? "通过" : "未通过"));
+
+    }
+
+    public static boolean test() {
+        int len = 100;
+        int max = 1000;
+        int testTimes = 5000;
+        int addOrUpdateTimes = 1000;
+        int queryTimes = 500;
+        for (int i = 0; i < testTimes; i++) {
+            int[] origin = genarateRandomArray(len, max);
+            SegmentTree seg = new SegmentTree(origin);
+            int S = 1;
+            int N = origin.length;
+            int root = 1;
+            seg.build(S, N, root);
+            Right rig = new Right(origin);
+            for (int j = 0; j < addOrUpdateTimes; j++) {
+                int num1 = (int) (Math.random() * N) + 1;
+                int num2 = (int) (Math.random() * N) + 1;
+                int L = Math.min(num1, num2);
+                int R = Math.max(num1, num2);
+                int C = (int) (Math.random() * max) - (int) (Math.random() * max);
+                if (Math.random() < 0.5) {
+                    seg.add(L, R, C, S, N, root);
+                    rig.add(L, R, C);
+                } else {
+                    seg.update(L, R, C, S, N, root);
+                    rig.update(L, R, C);
+                }
+            }
+            for (int k = 0; k < queryTimes; k++) {
+                int num1 = (int) (Math.random() * N) + 1;
+                int num2 = (int) (Math.random() * N) + 1;
+                int L = Math.min(num1, num2);
+                int R = Math.max(num1, num2);
+                long ans1 = seg.query(L, R, S, N, root);
+                long ans2 = rig.query(L, R);
+                if (ans1 != ans2) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+}
