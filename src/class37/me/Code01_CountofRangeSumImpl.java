d6e7e02aaccda9161a4163e37c9e89023f615f16
xionglei <xionglei1614@163.com>
2023-04-06 16:00:04 +0800

    add me code: count of range sum 归并写法done, 线段树解法待学习

--- src/class37/me/Code01_CountofRangeSumImpl.java
+++ src/class37/me/Code01_CountofRangeSumImpl.java
@@ -4,12 +4,9 @@ import class37.Code01_CountofRangeSum;
 
 public class Code01_CountofRangeSumImpl extends Code01_CountofRangeSum {
 
-
     // 求数组arr的子数组累加和在range范围（闭区间）的个数range [low, high]
-
-    // 思路与转换：累加和 => 前缀和数组sum + 已i结尾的子数组中，有哪些前缀和在range范围中
+    // 思路与转换：累加和(注意整形越界) => 前缀和数组sum + 已i结尾的子数组中，有哪些前缀和在range范围中
     // i > j, 若sum[i] - sum[j] in [low, high], 则arr中[j + 1, j]区间和in[low, high]
-
     public static void main(String[] args) {
         int len = 6;
         int varible = 50;
@@ -18,7 +15,7 @@ public class Code01_CountofRangeSumImpl extends Code01_CountofRangeSum {
             int lower = (int) (Math.random() * varible) - (int) (Math.random() * varible);
             int upper = lower + (int) (Math.random() * varible);
             int ans1 = countRangeSum1(test, lower, upper);
-            int ans2 = new CountOfRangeSumByMergeSort().countOfRangeSum(test, lower, upper);
+            int ans2 = new CountOfRangeSumSegTree().countOfRangeSum(test, lower, upper);
             if (ans1 != ans2) {
                 printArray(test);
                 System.out.println(lower);
@@ -33,99 +30,213 @@ public class Code01_CountofRangeSumImpl extends Code01_CountofRangeSum {
 
     }
 
-    public static class CountOfRangeSumBySortedMap {
 
-        private static class SBTreeMap<K extends Comparable<K>, V> {
+    private static class CountOfRangeSumMergeSort {
 
-            private SBNode<K, V> rightRotate(SBNode<K, V> cur) {
-                SBNode<K, V> l = cur.l;
-                cur.l = l.r;
-                l.r = cur;
-                // TODO Lei: 维护size
-                return l;
-            }
-
-
-            private static class SBNode<K extends Comparable<K>, V> {
-                K k;
-                V v;
-                int size;
-                SBNode<K, V> l, r;
-
-                public SBNode(K k, V v) {
-                    this.k = k;
-                    this.v = v;
-                    this.size = 1;
-                }
-            }
-        }
-    }
-
-    public static class CountOfRangeSumByMergeSort {
-        // 利用归并merge过程两边有序，使比较不回退，降低时间复杂度
-        // 方法一：归并排序
-        public int countOfRangeSum(int[] arr, int low, int high) {
-            if (arr == null) return 0;
+        public int countOfRangeSum(int[] arr, int low, int upper) {
+            if (arr == null || upper < low) return 0;
             int N = arr.length;
-            long[] sum = new long[N];
-            sum[0] = arr[0];
-            for (int i = 1; i < N; i++) {
-                sum[i] = sum[i - 1] + arr[i];
+            long[] sum = new long[N + 1];
+            for (int i = 0; i < N; i++) {
+                sum[i + 1] = sum[i] + arr[i];
             }
-            return process(sum, 0, N - 1, low, high);
+            // int[] sum = new int[N];
+            // sum[0] = arr[0];
+            // for (int i = 1; i < N; i++) {
+            //     sum[i] = sum[i - 1] + arr[i];
+            // }
+            return mergeSort(sum, 0, N, low, upper);
         }
 
-        private int process(long[] arr, int left, int right, int low, int high) {
+        private int mergeSort(long[] sum, int left, int right, int low, int upper) {
             if (left == right) {
-                return arr[left] >= low && arr[left] <= high ? 1 : 0;
+                // 单独自身已经在sum[right] - sum[0]处理
+                return 0;
+//                return sum[left] >= low && sum[right] <= upper ? 1 : 0;
             }
+            // 问题转换为分别已当前？结尾的子数组累加和在[low, upper]内
+            // 利用merge的有序，窗口不回退
             int mid = left + ((right - left) >> 1);
-            int p1 = process(arr, left, mid, low, high);
-            int p2 = process(arr, mid + 1, right, low, high);
-            int p3 = merge(arr, left, mid, right, low, high);
-            return p1 + p2 + p3;
+            return mergeSort(sum, left, mid, low, upper) +
+                    mergeSort(sum, mid + 1, right, low, upper) +
+                    merge(sum, left, mid, right, low, upper);
         }
 
-        private int merge(long[] sum, int left, int mid, int right, int low, int high) {
-            int l1 = left, l2 = left, r = mid + 1;
-            long min, max;
-            // j < i; sum[i] - sum[j] in [low, high]
-            // sum[j] in [sum[i] - high, sum[i] - low]
-            // sum[j] 递增  [sum[i] - high, sum[i] - low] 递增， 不回退， 窗口
-            int ans = 0;
-            while (r <= right) {
-                min = sum[r] - high;
-                max = sum[r] - low;
-                while (l1 <= mid && sum[l1] < min) {
+        private int merge(long[] sum, int left, int mid, int right, int low, int upper) {
+            int l1 = left, l2 = left;
+            // sum[r] - sum[l] in [low, upper]
+            // sum[l] in  [sum[r] - upper, sum[r] - low]
+            int count = 0;
+            for (int r = mid + 1; r <= right; r++) {
+                while (l1 <= mid && sum[l1] < sum[r] - upper) {
                     l1++;
                 }
-                while (l2 <= mid && sum[l2] <= max) {
+                while (l2 <= mid && sum[l2] <= sum[r] - low) {
                     l2++;
                 }
-                ans += l2 - l1;
-                r++;
+                count += l2 - l1;
             }
             // merge
-            int p1 = left;
-            int p2 = mid + 1;
+            int p1 = left, p2 = mid + 1, i = 0;
             long[] help = new long[right - left + 1];
-            int i = 0;
-            while (p1 <= mid && p2 <= right) {
+            while (p1 <= mid && p2 <= right)
                 help[i++] = sum[p1] <= sum[p2] ? sum[p1++] : sum[p2++];
-            }
-            while (p1 <= mid) {
+            while (p1 <= mid)
                 help[i++] = sum[p1++];
-            }
-            while (p2 <= right) {
+            while (p2 <= right)
                 help[i++] = sum[p2++];
-            }
-            while (--i >= 0) {
-                sum[left + i] = help[i];
-            }
-            return ans;
+            System.arraycopy(help, 0, sum, left, help.length);
+            return count;
         }
 
     }
+    
+    private static class CountOfRangeSumSegTree {
+
+        public int countOfRangeSum(int[] arr, int lower, int upper) {
+            if (arr == null || upper < lower) return 0;
+            int N = arr.length;
+            long[] sum = new long[N + 1];
+            for (int i = 0; i < N; i++) {
+                sum[i + 1] = sum[i] + arr[i];
+            }
+            SegmentTree sg = new SegmentTree(N);
+            // [0, N - 1] -> [1, N] 
+            // 0(N2LogN) gg
+            int count = 0;
+            for (int i = 1; i <= N; i++) {
+                sg.add(i, i, arr[i - 1], 1, N, 1);
+                for (int start = 1; start <= i; start++) {
+                    long query = sg.query(start, i, 1, N, 1);
+                    if (query >= lower && query <= upper) {
+                        count++;
+                    }
+                }
+            }
+            return count;
+        }
+
+        private static class SegmentTree {
+            int MAX;
+            int[] arr;
+            long[] sum;
+            int[] lazy;
+            int[] change;
+            boolean[] update;
+
+            public SegmentTree(int[] origin) {
+                this.MAX = origin.length + 1;
+                this.arr = new int[MAX];
+                System.arraycopy(origin, 0, this.arr, 1, origin.length);
+                this.sum = new long[MAX << 2];
+                this.lazy = new int[MAX << 2];
+                this.change = new int[MAX << 2];
+                this.update = new boolean[MAX << 2];
+            }
+
+            public SegmentTree(int len) {
+                this.MAX = len + 1;
+                this.arr = new int[MAX];
+                this.sum = new long[MAX << 2];
+                this.lazy = new int[MAX << 2];
+                this.change = new int[MAX << 2];
+                this.update = new boolean[MAX << 2];
+            }
+
+            private void pushup(int rt) {
+                sum[rt] = sum[rt << 1] + sum[rt << 1 | 1];
+            }
+
+            private void pushdown(int rt, int ln, int rn) {
+                if (update[rt]) {
+                    update[rt << 1] = true;
+                    update[rt << 1 | 1] = true;
+                    change[rt << 1] = change[rt];
+                    change[rt << 1 | 1] = change[rt];
+                    lazy[rt << 1] = 0;
+                    lazy[rt << 1 | 1] = 0;
+                    sum[rt << 1] = (long) change[rt] * ln;
+                    sum[rt << 1 | 1] = (long) change[rt] * rn;
+                    update[rt] = false;
+                }
+                if (lazy[rt] != 0) {
+                    sum[rt << 1] += (long) lazy[rt] * ln;
+                    sum[rt << 1 | 1] += (long) lazy[rt] * rn;
+                    lazy[rt << 1] += lazy[rt];
+                    lazy[rt << 1 | 1] += lazy[rt];
+                    lazy[rt] = 0;
+                }
+            }
+
+            public void build(int l, int r, int rt) {
+                if (l == r) {
+                    sum[rt] = arr[l];
+                    return;
+                }
+                int mid = l + ((r - l) >> 1);
+                build(l, mid, rt << 1);
+                build(mid + 1, r, rt << 1 | 1);
+                pushup(rt);
+            }
+
+            // 原[L~R]范围上的元素增加C
+            public void add(int L, int R, int C, int l, int r, int rt) {
+                if (L <= l && r <= R) {
+                    sum[rt] += (long) C * (r - l + 1);
+                    lazy[rt] += C;
+                    return;
+                }
+                int mid = l + ((r - l) >> 1);
+                pushdown(rt, mid - l + 1, r - mid);
+                if (L <= mid) {
+                    add(L, R, C, l, mid, rt << 1);
+                }
+                if (R > mid) {
+                    add(L, R, C, mid + 1, r, rt << 1 | 1);
+                }
+                pushup(rt);
+            }
+
+            // 原[L~R]范围上的元素变为C
+            public void update(int L, int R, int C, int l, int r, int rt) {
+                if (L <= l && r <= R) {
+                    sum[rt] = (long) C * (r - l + 1);
+                    update[rt] = true;
+                    change[rt] = C;
+                    lazy[rt] = 0;
+                    return;
+                }
+                int mid = l + ((r - l) >> 1);
+                pushdown(rt, mid - l + 1, r - mid);
+                if (L <= mid) {
+                    update(L, R, C, l, mid, rt << 1);
+                }
+                if (R > mid) {
+                    update(L, R, C, mid + 1, r, rt << 1 | 1);
+                }
+                pushup(rt);
+            }
+
+            // 查询原[L~R]范围的累加和
+            public long query(int L, int R, int l, int r, int rt) {
+                if (L <= l && r <= R) {
+                    return sum[rt];
+                }
+                int mid = l + ((r - l) >> 1);
+                pushdown(rt, mid - l + 1, r - mid);
+                int res = 0;
+                if (L <= mid) {
+                    res += query(L, R, l, mid, rt << 1);
+                }
+                if (R > mid) {
+                    res += query(L, R, mid + 1, R, rt << 1 | 1);
+                }
+                // 不需要pushup, query当前sum[rt]的值不变
+                // pushup(rt);
+                return res;
+            }
 
 
+        }
+    }
 }
