a57073e54f0c6d29f6a8b11c9c216e8108c7fe17
xionglei <xionglei1614@163.com>
2023-04-11 11:09:26 +0800

    add me code: 窗口内最大值最小值更新结构

--- src/class24/me/Code01_SlidingWindowMaxArrayImpl.java
+++ src/class24/me/Code01_SlidingWindowMaxArrayImpl.java
@@ -1,17 +1,14 @@
 package class24.me;
 
 import class24.Code01_SlidingWindowMaxArray;
+import cn.hutool.core.util.StrUtil;
 
-import java.util.Arrays;
 import java.util.Deque;
 import java.util.LinkedList;
 
 public class Code01_SlidingWindowMaxArrayImpl extends Code01_SlidingWindowMaxArray {
 
     public static void main(String[] args) {
-        int[] right = getMaxWindow(new int[]{1, -1}, 1);
-        System.out.println(Arrays.toString(right));
-
         int testTime = 100000;
         int maxSize = 100;
         int maxValue = 100;
@@ -20,9 +17,11 @@ public class Code01_SlidingWindowMaxArrayImpl extends Code01_SlidingWindowMaxArr
             int[] arr = generateRandomArray(maxSize, maxValue);
             int w = (int) (Math.random() * (arr.length + 1));
             int[] ans1 = getMaxWindow(arr, w);
-            int[] ans2 = right(arr, w);
+            int[] ans2 = new SlidingWindowMax().maxSlidWindow(arr, w);
             if (!isEqual(ans1, ans2)) {
+                System.out.println(StrUtil.format("ans1: {}, ans2: {}", ans1, ans2));
                 System.out.println("Oops!");
+                break;
             }
         }
         System.out.println("test finish");
@@ -30,22 +29,50 @@ public class Code01_SlidingWindowMaxArrayImpl extends Code01_SlidingWindowMaxArr
 
     public static class SlidingWindowMax {
 
-        public int[] maxSlidingWindow(int[] nums, int K) {
-            if (nums == null || K < 1 || nums.length < 1) return null;
-            int N = nums.length;
+        public int[] maxSlidWindow(int[] arr, int K) {
+            if (arr == null || arr.length < K || K < 1) return null;
+            int N = arr.length;
+            int[] ans = new int[N - K + 1];
+            LinkedList<Integer> winMax = new LinkedList<>();
+            for (int i = 0; i < N; i++) {
+                while (!winMax.isEmpty() && arr[winMax.peekLast()] <= arr[i]) {
+                    winMax.pollLast();
+                }
+                winMax.offerLast(i);
+                if (i - K == winMax.peekFirst()) {
+                    winMax.pollFirst();
+                }
+                // [0, K - 1] -> K
+                if (i >= K - 1) {
+                    ans[i - K + 1] = arr[winMax.peekFirst()];
+                }
+            }
+            return ans;
+        }
+        
+        
+        
+        
+        
+
+
+        public int[] maxSlidingWindow(int[] arr, int K) {
+            if (arr == null || K < 1 || arr.length < 1) return null;
+            int N = arr.length;
             Deque<Integer> qmax = new LinkedList<>();
             int[] ans = new int[N - K + 1];
             int index = 0;
-            for (int R = 0; R < N; R++) {
-                while (!qmax.isEmpty() && qmax.peekLast() <= nums[R]) {
+            for (int r = 0; r < N; r++) {
+                while (!qmax.isEmpty() && arr[qmax.peekLast()] <= arr[r]) {
                     qmax.pollLast();
                 }
-                qmax.offerLast(nums[R]);
-                if (qmax.peekFirst() == R - K) {
+                qmax.offerLast(r);
+                // 窗口左边界
+                if (qmax.peekFirst() == r - K) {
                     qmax.pollFirst();
                 }
-                if (R >= K - 1) {
-                    ans[index++] = qmax.peekFirst();
+                if (r >= K - 1) {
+                    ans[index++] = arr[qmax.peekFirst()];
                 }
             }
             return ans;
